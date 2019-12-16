1f09c9e16f83ce0333327d047ec78d453a2be2b3
Michael J. Simons <michael@simons.ac>
2019-12-16 14:40:49 +0100

    Remove start-class property and enable all the logging.

--- src/main/java/org/neo4j/examples/drivernative/DriverNativeApplication.java
+++ src/main/java/org/neo4j/examples/drivernative/DriverNativeApplication.java
@@ -2,10 +2,13 @@ package org.neo4j.examples.drivernative;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
 
 import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Logging;
 import org.neo4j.driver.Session;
 import org.neo4j.driver.SessionConfig;
 import org.neo4j.driver.async.AsyncSession;
@@ -13,7 +16,8 @@ import org.neo4j.driver.async.AsyncSession;
 public class DriverNativeApplication {
 
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
-		try (Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "secret"))) {
+		Config config = Config.builder().withLogging(Logging.console(Level.ALL)).build();
+		try (Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "secret"), config)) {
 			try (Session session = driver.session()) {
 				session.run("MATCH (m:Movie) RETURN m.title AS title")
 					.list(r -> r.get("title").asString())
